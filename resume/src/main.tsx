7c595b109d96a72546c3f1d23065b9dd2c706b87
Monke72 <wera16307@gmail.com>
2025-06-15 23:00:31 +0400

    "chore" - deploy

--- resume/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./styles/index.scss";
-import "../public/locales";
-
-createRoot(document.getElementById("root")!).render(<App />);
--- resume/src/types/IconTypes.ts
+++
@@ -1,4 +0,0 @@
-export interface IIcons {
-  src: string;
-  alt: string;
-}
